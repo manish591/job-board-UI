e5b43c1dfbe6616f795feb7c603474fa1af6e223
Manish Devrani <manishdevrani777@gmail.com>
2024-06-27 00:34:06 +0530

    added css variable colors in tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,27 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+      colors: {
+        brand: "var(--brand)",
+        bgPrimary: "var(--bg-primary)",
+        bgSecondary: "var(--bg-secondary)",
+        bgGhost: "var(--bg-ghost)",
+
+        textBlack900: "var(--text-black-900)",
+        textBlack800: "var(--text-black-800)",
+        textBlack700: "var(--text-black-700)",
+        textBlack600: "var(--text-black-600)",
+        textBlack500: "var(--text-black-500)",
+        textBlack400: "var(--text-black-400)",
+        textBlack300: "var(--text-black-300)",
+
+        textGreen100: "var(--text-green-100)",
+        textGreen200: "var(--text-green-200)",
+        textGreen300: "var(--text-green-300)",
+
+        border100: "var(--border-100)",
+        border200: "var(--border-200)"
+      }
     },
   },
   plugins: [],
